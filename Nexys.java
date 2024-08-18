e8e8708d8a4662adf202c6b402136794a7d8bc00
Satish Gavhane <satishgavhane2909@gmail.com>
2024-08-18 21:44:37 +0530

    sd

--- Nexys.java
+++ Nexys.java
@@ -2,6 +2,7 @@ public class Nexys {
     public static void main(String[] args) {
         int number = 12;
         System.out.println(number);
-        System.out.println("Numer");
+        System.out.println("Bug");
+        
     }
 }
