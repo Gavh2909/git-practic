20a77548d04fc9716268de2f3d5b0c0e53c60789
SATISH GAVHANE <87062915+Gavh2909@users.noreply.github.com>
2024-08-19 21:56:32 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,24 +2,9 @@
 class Main{
     public static void main(String[] args) {
         getName();
+        Hello
     }
 
-    public static void getName(){
-        System.out.println("Satish Gavhane");
-    }
-
-    public static int getAge(){
-       return 33;
-    }
-
-    // This is changed in MAin branch
-    String main = "Changed";
-
-    public static boolean isTrue(){
-        return true;
-    }
-
-    // This is added in feature branch 
-    int num = 10903;
+    
 }
 
