5b6ae6fd592f80e09a3f47957b88ee3401614cf2
Satish Gavhane <satishgavhane2909@gmail.com>
2024-08-19 21:37:08 +0530

    getName method created

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 class Main{
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(1,22,3,4,5,6,76));
-        int k = 11;
         
-        for(int n: list){
-            if(list.contains(k-n)){
-                System.out.println(n+" ");
-                System.out.println(k-n+" ");
-            }
-        }
+    }
+
+    public void getName(){
+        System.out.println("Satish Gavhane");
     }
 }
 
-// From a list of  number with duplicates idenity pair of two adjoining number whose sum is equal to given number
--- Nexys.java
+++
@@ -1,9 +0,0 @@
-public class Nexys {
-    public static void main(String[] args) {
-        int number = 12;
-        System.out.println(number);
-
-        System.out.println("Number");
-
-    }
-}
